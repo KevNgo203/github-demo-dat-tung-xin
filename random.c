decea172b1fe5504e65525cd42f448527e130e22
Kevin Ngo <quocdatngo2003@gmail.com>
2023-09-15 23:37:57 +1000

    update random.c

--- random.c
+++ random.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+    return a + b;
+}
 
 int main(void) {
 
-    printf("Wassup everybody!!!!!");
+    printf("The addition of 4 and 5 is %d\n", add(4,5));
 
     return 0;
 }
